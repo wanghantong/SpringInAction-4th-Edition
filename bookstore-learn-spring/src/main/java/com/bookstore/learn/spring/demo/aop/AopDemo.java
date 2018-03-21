19a6af35fe2584e76fdd857b3235c5233cd83251
dragon <hantong4510@163.com>
2018-03-21 14:41:24 +0800

    commit..

--- bookstore-learn-spring/src/main/java/com/bookstore/learn/spring/demo/aop/AopDemo.java
+++ bookstore-learn-spring/src/main/java/com/bookstore/learn/spring/demo/aop/AopDemo.java
@@ -1,9 +1,15 @@
 package com.bookstore.learn.spring.demo.aop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AopDemo {
 
 	public void printResult() {
 		System.err.println("this is aop....");
+
+		List<String> list = new ArrayList<String>();
+
 	}
 
 }
